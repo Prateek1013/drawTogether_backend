d520ab7e5d293b638dd8f0cb6b85c53991697bc1
Prateek1013 <prateek9771196112@gmail.com>
2023-12-30 01:12:07 +0530

    socket setup client+server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  // ...
+  console.log("server connected");
+});
+
+httpServer.listen(4000);
