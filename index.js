a7e448040b66bdd3ea79824b5844f84896b27715
Prateek1013 <prateek9771196112@gmail.com>
2024-01-05 01:39:05 +0530

    brodcast

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ app.use(cors());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000", //frontend
   },
 });
 
 io.on("connection", (socket) => {
-  // ...
+  socket.on("beginPath", (arg) => {
+    socket.broadcast.emit("anything_path", arg);
+  });
+  socket.on("drawPath", (arg) => {
+    socket.broadcast.emit("anything_draw", arg);
+  });
+  socket.on("changeconfig", (arg) => {
+    socket.broadcast.emit("any_changeconfig", arg);
+  });
   console.log("server connected");
 });
 
